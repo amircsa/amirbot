9f30d7f211534314954965d951ebc55971699bb9
amiroo126m <seyeda.126@gmail.com>
2016-09-07 18:35:05 +0430

    Update seedbot.lua

--- bot/seedbot.lua
+++ bot/seedbot.lua
@@ -226,7 +226,7 @@ function create_config( )
     "leave_ban",
     "admin"
     },
-    sudo_users = {@amir126m},--Sudo users
+    sudo_users = {75321713},--Sudo users
     disabled_channels = {},
     moderation = {data = 'data/moderation.json'},
     about_text = [[Teleseed v2 - Open Source
